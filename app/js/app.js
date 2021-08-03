89273da6205d7a35a944e811c72db2230594f10e
maxim_koryakov <maxim.koryakov@gmail.com>
2021-08-03 17:29:37 +0300

    benefit, directions, form, results, rewiew complite!

--- app/js/app.js
+++ app/js/app.js
@@ -8,6 +8,51 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-	// Custom JS
+	var slider = tns({
+    container: '.results_slider',
+    items: 2,
+    speed: 400,
+    mouseDrag: true,
+    slideBy: 'page',
+    navPosition: 'bottom',
+    controlsText: ["🠨", "🠪"],
+    responsive: {
+      640: {
+        edgePadding: 20,
+        gutter: 20,
+        items: 2,
+      },
+      700: {
+        gutter: 40
+      },
+      900: {
+        items: 2
+      }
+    }
+  });
+
+
+  var slider = tns({
+    container: '.review_slider',
+    items: 2,
+    speed: 400,
+    mouseDrag: true,
+    slideBy: 'page',
+    navPosition: 'bottom',
+    controlsText: ["🠨", "🠪"],
+    responsive: {
+      640: {
+        edgePadding: 20,
+        gutter: 20,
+        items: 2,
+      },
+      700: {
+        gutter: 40
+      },
+      900: {
+        items: 2
+      }
+    }
+  });
 
 })
